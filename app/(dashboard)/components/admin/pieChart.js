7a9103fdd8d3fdc25024e0e4ebcc47c91351dffe
Mahedi-Hasan10 <hasanmdmahedi193@gmail.com>
2024-05-08 09:13:48 +0600

    updated

--- app/(dashboard)/components/admin/pieChart.js
+++ app/(dashboard)/components/admin/pieChart.js
@@ -1,7 +1,7 @@
-'use client'
-import React, { useState } from 'react';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Pie } from 'react-chartjs-2';
+"use client";
+import React, { useState } from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -9,32 +9,29 @@ const PieChart = () => {
   const data = {
     datasets: [
       {
-        
-        data: [12, 19, 3, 5, 2, 3],
+        data: [30, 20, 15, 10, 17, 8],
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
+          " #6d48ff",
+          "#E4DDFE",
+          "#4ECB71",
+          "#FFD233",
+          "#ff9a62",
+          "#d99bff",
         ],
         borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
         ],
         borderWidth: 1,
       },
     ],
   };
 
-  return (
-     <Pie data={data} className='md:!h-[300px] !h-[200px]' />
-  );
+  return <Pie data={data} className="md:!h-[300px] !h-[200px]" />;
 };
 
 export default PieChart;
